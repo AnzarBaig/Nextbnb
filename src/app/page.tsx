5310c0f1bb1b5ea011d30f4ba84f7dfbe993d8da
Mirza Anzar Baig <mabaig222@gmail.com>
2023-07-12 13:47:28 +0530

    Resolved Mege conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-rose-500 text-2xl">
+        hehehehhe
+      </h1>
+    </div>
+  )
+}
 import Image from 'next/image'
 
 export default function Home() {
