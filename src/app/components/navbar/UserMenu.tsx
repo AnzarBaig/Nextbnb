9068c089206af90eb2795a905936dbe4f11cf2b8
Mirza Anzar Baig <mabaig222@gmail.com>
2023-07-15 00:27:25 +0530

    Added Login and Logout functionality

--- src/app/components/navbar/UserMenu.tsx
+++ src/app/components/navbar/UserMenu.tsx
@@ -4,8 +4,17 @@ import Avatar from "../Avatar";
 import { useState, useCallback } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "../hooks/useRegisterModel";
-const UserMenu = () => {
+import useLoginModal from "../hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { createSemanticDiagnosticsBuilderProgram } from "typescript";
+import {signOut} from 'next-auth/react'
+interface UserMenuProps {
+  currentUser?: User | null
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
   const registerModal = useRegisterModal(); 
+  const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -34,10 +43,23 @@ const UserMenu = () => {
       {isOpen && (
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-1/4 bg-white overflow-hidden right-10 top-15 text-sm">
           <div className="flex flex-col cursor-pointer">
+            {currentUser ? (
+              <>
+              <MenuItem onClick={() => {}} label="My Trips" />
+              <MenuItem onClick={() => {}} label="My Favorites"/>
+              <MenuItem onClick={() => {}} label="My Reservations"/>
+              <MenuItem onClick={() => {}} label="My Properties"/>
+              <MenuItem onClick={() => {}} label="NextBNB My Home"/>
+              <hr />
+              <MenuItem onClick={() => signOut()} label="Logout"/>
+            </>
+
+            ) : (
             <>
-              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={loginModal.onOpen} label="Login" />
               <MenuItem onClick={registerModal.onOpen} label="Signup" />
             </>
+            )}
           </div>
         </div>
       )}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import Navbar from "./components/navbar/Navbar";
 
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./components/providers/ToasterProvider";
-
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 const inter = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -13,17 +14,19 @@ export const metadata: Metadata = {
   description: "Almost clone of AirBnb",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang="en">
       <body className={inter.className}>
         <ToasterProvider/>
+        <LoginModal/>
         <RegisterModal/>
-        <Navbar/>
+        <Navbar currentUser = {currentUser} />
         {children}
         </body>
     </html>
