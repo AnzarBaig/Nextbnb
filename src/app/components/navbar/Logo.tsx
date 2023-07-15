737c3b1c3699bccc2cafb2b28bd633dc2eb371e0
Mirza Anzar Baig <mabaig222@gmail.com>
2023-07-15 22:21:50 +0530

    My commit message

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
+    const router = useRouter();
     return ( 
 
-        <Image alt = "Logo" className="hidden md:block cursor-pointer" src= "/images/logo.png" height="100" width="100"/>
+        <Image onClick={() => router.push('/')} alt = "Logo" className="hidden md:block cursor-pointer" src= "/images/logo.png" height="100" width="100"/>
 
      );
 }
